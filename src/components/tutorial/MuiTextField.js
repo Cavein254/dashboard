c17020f010c1a0345bfe972f312726175442f000
John Muthua <caveomuthama@gmail.com>
2023-07-05 14:56:18 +0300

    Added adorment to the text field

--- src/components/tutorial/MuiTextField.js
+++ src/components/tutorial/MuiTextField.js
@@ -1,12 +1,40 @@
-import { Stack, TextField } from '@mui/material';
+import { InputAdornment, Stack, TextField } from '@mui/material';
 import React from 'react';
 const MuiTextField = () => {
   return (
     <Stack spacing={4}>
-      <Stack direction="row" spacing={10}>
+      <Stack orientation="vertical" spacing={10}>
         <TextField label="username" variant="outlined" />
         <TextField label="username" variant="filled" />
-        <TextField label="username" variant="standard" />
+        <TextField label="username" variant="outlined" />
+
+        <TextField
+          label="username"
+          variant="standard"
+          size="small"
+          color="secondary"
+        />
+
+        <TextField
+          variant="outlined"
+          label="password"
+          helperText="Don not share your password"
+          type="password"
+        />
+      </Stack>
+      <Stack direction="row">
+        <TextField
+          label="Amount"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="Weight"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">Kgs</InputAdornment>,
+          }}
+        />
       </Stack>
     </Stack>
   );
