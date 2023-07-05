cb7fb296160735750150612898824ac82306cb5d
John Muthua <caveomuthama@gmail.com>
2023-07-05 08:54:25 +0300

    colors with several levels

--- src/components/tutorial/MuiButton.jsx
+++ src/components/tutorial/MuiButton.jsx
@@ -1,13 +1,34 @@
-import { Button } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import React from 'react';
 
 const MuiButton = () => {
   return (
-    <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </div>
+    <Stack spacing={8}>
+      <Stack spacing={4} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack direction="row" spacing={4}>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+    </Stack>
   );
 };
 
