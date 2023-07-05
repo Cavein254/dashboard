ecd99376211ebe1f9011fd0f5f50a2de92fe15f3
John Muthua <caveomuthama@gmail.com>
2023-07-05 08:35:58 +0300

    Added typography

---
+++ src/components/tutorial/MuiTypography.jsx
@@ -0,0 +1,20 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 variant</Typography>
+      <Typography variant="h2">h2 variant</Typography>
+      <Typography variant="h3">h3 variant</Typography>
+      <Typography variant="h4">h4 variant</Typography>
+      <Typography variant="h5">h5 variant</Typography>
+      <Typography variant="h6">h6 variant</Typography>
+
+      <Typography variant="subtittle1">sub title one</Typography>
+      <Typography variant="subtittle2">sub title two</Typography>
+    </div>
+  );
+};
+
+export default MuiTypography;
