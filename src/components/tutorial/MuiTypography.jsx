f868651039526faaf33b37c6561a0a50b782d6b0
John Muthua <caveomuthama@gmail.com>
2023-07-05 08:38:53 +0300

    Added body typography

--- src/components/tutorial/MuiTypography.jsx
+++ src/components/tutorial/MuiTypography.jsx
@@ -13,6 +13,20 @@ const MuiTypography = () => {
 
       <Typography variant="subtittle1">sub title one</Typography>
       <Typography variant="subtittle2">sub title two</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic ex tempora
+        voluptatem voluptate maiores, aliquid laboriosam blanditiis quia aliquam
+        nam distinctio, magni quasi architecto, eaque corporis illum consectetur
+        quibusdam recusandae?
+      </Typography>
+
+      <Typography variant="body2">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic ex tempora
+        voluptatem voluptate maiores, aliquid laboriosam blanditiis quia aliquam
+        nam distinctio, magni quasi architecto, eaque corporis illum consectetur
+        quibusdam recusandae?
+      </Typography>
     </div>
   );
 };
