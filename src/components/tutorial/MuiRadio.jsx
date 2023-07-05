509ab1fc8c8c9dbca07f6b432cb2d49bd150a0f8
John Muthua <caveomuthama@gmail.com>
2023-07-05 15:45:20 +0300

    Added radio options

--- src/components/tutorial/MuiRadio.jsx
+++ src/components/tutorial/MuiRadio.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
+import React, { useState } from 'react';
 
 const MuiRadio = () => {
-  return <div>MuiRadio</div>;
+  const [value, setValue] = useState('');
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    console.log({ value });
+  };
+  return (
+    <Box>
+      <FormControl>
+        <FormLabel id="job-experience">Years of Experience</FormLabel>
+        <RadioGroup
+          id="job-experience"
+          aria-labelledby="job-experience"
+          onChange={handleChange}
+          row
+        >
+          <FormControlLabel control={<Radio />} label="0-12" value="0-12" />
+          <FormControlLabel control={<Radio />} label="3-12" value="3-12" />
+          <FormControlLabel control={<Radio />} label="10-12" value="10-12" />
+        </RadioGroup>
+      </FormControl>
+    </Box>
+  );
 };
 
 export default MuiRadio;
