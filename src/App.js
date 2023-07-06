0676b95559a7b5f122fbe5da8d9d085fc3b56eeb
John Muthua <caveomuthama@gmail.com>
2023-07-06 17:46:32 +0300

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from './scenes/dashboard';
+import { Routes } from 'react-router-dom';
+// import Dashboard from './scenes/dashboard';
+import Sidebar from './scenes/globals/Sidebar';
 import Topbar from './scenes/globals/Topbar';
 import { ColorModeContext, useMode } from './theme';
+// import Team from './scenes/team';
+// import Invoices from './scenes/invoices';
+// import Contacts from './scenes/contacts';
+// import Bar from './scenes/bar';
+// import Form from './scenes/form';
+// import Line from './scenes/line';
+// import Pie from './scenes/pie';
+// import FAQ from './scenes/faqs';
+// import Geography from './scenes/geography';
+// import Calender from './scenes/calender';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -13,8 +24,19 @@ function App() {
         <div className="app">
           <main className="content">
             <Topbar />
+            <Sidebar />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/" element={<Dashboard />} /> */}
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/faqs" element={<FAQ />} /> */}
+              {/* <Route path="/geo" element={<Geography />} /> */}
+              {/* <Route path="/calender" element={<Calender />} /> */}
             </Routes>
           </main>
         </div>
