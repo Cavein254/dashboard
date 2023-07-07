42b091da68047448a7c4f3d9f3ee0983e7ac76b2
John Muthua <caveomuthama@gmail.com>
2023-07-07 15:13:01 +0300

    Add all members

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
-
+import { Box, useTheme } from '@mui/material';
+// import { DataGrid } from '@mui/x-data-grid';
+import { tokens } from '../../theme';
+// import {mockDataTeam} from '../../data/mockData';
+// import AdminPanelSettingsOutlinedIcon  from '@mui/icons-material/AdminPanelSettingsOutlined';
+// import LockOpenOutlinedIcon  from '@mui/icons-material/LockOpenOutlined';
+// import SecurityOpenOutlinedIcon  from '@mui/icons-material/SecurityOutlined';
+import Header from '../../components/Header';
 const Team = () => {
-  return <div>Team</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box>
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+    </Box>
+  );
 };
 
 export default Team;
