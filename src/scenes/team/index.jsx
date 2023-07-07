3e07ac252cbcefbdb61a33f026de8b41ce3cb9be
John Muthua <caveomuthama@gmail.com>
2023-07-07 15:30:58 +0300

    Add teams

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,17 +1,62 @@
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+import SecurityOpenOutlinedIcon from '@mui/icons-material/SecurityOutlined';
 import { Box, useTheme } from '@mui/material';
-// import { DataGrid } from '@mui/x-data-grid';
-import { tokens } from '../../theme';
-// import {mockDataTeam} from '../../data/mockData';
-// import AdminPanelSettingsOutlinedIcon  from '@mui/icons-material/AdminPanelSettingsOutlined';
-// import LockOpenOutlinedIcon  from '@mui/icons-material/LockOpenOutlined';
-// import SecurityOpenOutlinedIcon  from '@mui/icons-material/SecurityOutlined';
+import { DataGrid } from '@mui/x-data-grid';
 import Header from '../../components/Header';
+import { mockDataTeam } from '../../data/mockData';
+import { tokens } from '../../theme';
 const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: 'id', headerName: 'ID' },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      cellClassName: 'name-column--cell',
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      headerAlign: 'left',
+      align: 'left',
+    },
+    { field: 'phone', headerName: 'Phone Number', flex: 1 },
+    { field: 'email', headerName: 'Email', flex: 1 },
+    {
+      field: 'access',
+      headerName: 'Access Level',
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === 'admin'
+                ? colors.greenAccent[600]
+                : colors.greenAccent[700]
+            }
+            borderRadius="5px"
+          >
+            {access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
+            {access === 'manager' && <SecurityOpenOutlinedIcon />}
+            {access === 'admin' && <LockOpenOutlinedIcon />}
+          </Box>
+        );
+      },
+    },
+  ];
   return (
     <Box>
       <Header title="TEAM" subtitle="Managing the Team Members" />
+      <DataGrid rows={mockDataTeam} columns={columns} />
     </Box>
   );
 };
