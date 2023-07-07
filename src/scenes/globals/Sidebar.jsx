d236951ac8aef8983c4bd755b31ed8b556b795c3
John Muthua <caveomuthama@gmail.com>
2023-07-07 14:31:36 +0300

    Added charts

--- src/scenes/globals/Sidebar.jsx
+++ src/scenes/globals/Sidebar.jsx
@@ -166,6 +166,15 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{
+                m: '15px 0 5px 20px',
+              }}
+            >
+              Charts
+            </Typography>
             <Item
               title="BarChart"
               to="/bar"
