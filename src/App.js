5b602725c1f8f5e9030a74997ff8b279deadfb7d
John Muthua <caveomuthama@gmail.com>
2023-07-18 12:52:41 +0300

    More commits

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React from 'react';
-// import MuiBox from './components/tutorial/MuiBox';
-// import MuiAutoComplete from './components/tutorial/MuiAutoComplete';
-// import MuiSwitch from './components/tutorial/MuiSwitch';
-// import MuiRadio from './components/tutorial/MuiRadio';
-// import MuiCheckBox from './components/tutorial/MuiCheckBox';
-// import MuiTextField from './components/tutorial/MuiTextField';
-// import MuiSelection from './components/tutorial/MuiSelection';
-// import MuiTypography from './components/tutorial/MuiTypography';
-// import MuiButton from './components/tutorial/MuiButton';
-import MuiStack from './components/tutorial/MuiStack';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <>
-      {/* <MuiTypography /> */}
-      {/* <MuiButton /> */}
-      {/* <MuiTextField /> */}
-      {/* <MuiSelection /> */}
-      {/* <MuiRadio /> */}
-      {/* <MuiCheckBox /> */}
-      {/* <MuiSwitch /> */}
-      {/* <MuiAutoComplete /> */}
-      {/* <MuiBox /> */}
-      <MuiStack />
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/Atpp.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import MuiBox from './components/tutorial/MuiBox';
+// import MuiAutoComplete from './components/tutorial/MuiAutoComplete';
+// import MuiSwitch from './components/tutorial/MuiSwitch';
+// import MuiRadio from './components/tutorial/MuiRadio';
+// import MuiCheckBox from './components/tutorial/MuiCheckBox';
+// import MuiTextField from './components/tutorial/MuiTextField';
+// import MuiSelection from './components/tutorial/MuiSelection';
+// import MuiTypography from './components/tutorial/MuiTypography';
+// import MuiButton from './components/tutorial/MuiButton';
+import MuiStack from './components/tutorial/MuiStack';
+
+function App() {
+  return (
+    <>
+      {/* <MuiTypography /> */}
+      {/* <MuiButton /> */}
+      {/* <MuiTextField /> */}
+      {/* <MuiSelection /> */}
+      {/* <MuiRadio /> */}
+      {/* <MuiCheckBox /> */}
+      {/* <MuiSwitch /> */}
+      {/* <MuiAutoComplete /> */}
+      {/* <MuiBox /> */}
+      <MuiStack />
+    </>
+  );
+}
+
+export default App;
